30020e8dd3567bf5919c525ba27855cf6624d325
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-08-04 18:05:23 -0600

    refact: :fire: delete redis

--- config/custom-environment-variables.ts
+++ config/custom-environment-variables.ts
@@ -5,11 +5,4 @@ export default {
   accessTokenPublicKey: 'JWT_ACCESS_TOKEN_PUBLIC_KEY',
   refreshTokenPrivateKey: 'JWT_REFRESH_TOKEN_PRIVATE_KEY',
   refreshTokenPublicKey: 'JWT_REFRESH_TOKEN_PUBLIC_KEY',
-
-  smtp: {
-    host: 'EMAIL_HOST',
-    pass: 'EMAIL_PASS',
-    port: 'EMAIL_PORT',
-    user: 'EMAIL_USER',
-  },
 };
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import express, { Response } from 'express';
 import config from 'config';
 import validateEnv from './utils/validateEnv';
 import { PrismaClient } from '@prisma/client';
-import redisClient from './utils/connectRedis';
 
 validateEnv();
 
@@ -13,10 +12,9 @@ const app = express();
 async function bootstrap() {
   // Testing
   app.get('/api/healthchecker', async (_, res: Response) => {
-    const message = await redisClient.get('try');
     res.status(200).json({
       status: 'success',
-      message,
+      message: 'server running ✅',
     });
   });
 
