2924aed8df411aca5799685504d4b80789aad90f
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-08-11 18:59:00 -0600

    chore(prisma): :sparkles: update seed

---
+++ prisma/defaultData.ts
@@ -0,0 +1,19 @@
+export const espacioTypes = ['laboratorio', 'aula']
+export const roles = ['estudiante', 'docente']
+// export const turnos = ['mañana', 'tarde', 'noche']
+
+export const duracionesClase = [
+  {
+    startTime: new Date(0, 0, 0, 7, 30),//inicio a receso
+    endTime: new Date(0, 0, 0, 9, 0),  
+  },
+  {
+    startTime: new Date(0, 0, 0, 9, 30), // receso a fin
+    endTime: new Date(0, 0, 0, 12, 0),  
+  },
+  {
+    startTime: new Date(0, 0, 0, 7, 30), // inicio a fin
+    endTime: new Date(0, 0, 0, 12, 0),  
+  }
+]
+
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,40 +1,57 @@
 import { PrismaClient } from '@prisma/client'
+import { duracionesClase, espacioTypes, roles } from './defaultData'
 
 const prisma = new PrismaClient()
 
-const espacioTypes = ['laboratorio', 'aula']
 
-const roles = ['estudiante', 'docente']
+async function main() {
+	espacioTypes.forEach(async (el) => {
+		const espacio = await prisma.espacioType.upsert({
+			where: { name: el },
+			update: {},
+			create: { name: el },
+		})
+		console.log('espacio', espacio)
+	})
 
-async function main(){
-  
-  espacioTypes.forEach(async (el) => {
-    const espacio = await prisma.espacioType.upsert({
-      where: { name: el },
-      update: {},
-      create: {
-        name: el
+	roles.forEach(async (el) => {
+		const rol = await prisma.rol.upsert({
+			where: { name: el },
+			update: {},
+			create: { name: el },
+		})
+		console.log('rol', rol)
+
+	})
+
+  // turnos.forEach(async (el) => {
+  //   const turno = await prisma.turno.upsert({
+  //     where: { name: el },
+  //     update: {},
+  //     create: { name: el },
+  //   })
+  //   console.log('turno', turno)
+  // })
+
+  duracionesClase.forEach(async (el) => {
+    const duracion = await prisma.duracion.upsert({
+      where: {
+        startTime_endTime: {
+          startTime: el.startTime,
+          endTime: el.endTime,
+        },
       },
-    })
-    console.log('espacio', espacio)
-  })
-
-  roles.forEach( async (el) => {
-    const rol = await prisma.rol.upsert({
-      where: { name: el},
       update: {},
-      create: {
-        name: el
-      }
-    })
-    console.log('rol', rol)
-  })
+      create: { startTime: el.startTime, endTime: el.endTime },
+    });
+    console.log('duración', duracion);
+  });
 }
 
 main()
-  .then(() => prisma.$disconnect())
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit();
-  });
+	.then(() => prisma.$disconnect())
+	.catch(async (e) => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit()
+	})
