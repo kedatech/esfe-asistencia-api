8785611178bbdabaf258e4cf59fcda3faa4f0735
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-08-14 13:23:58 -0600

    feat: :sparkles: estudiante services

---
+++ src/services/external/estudiante.services.ts
@@ -0,0 +1,36 @@
+import { esfeapi } from './config'
+import { returnProvider, IReturn} from '../../utils/ReturnProvider'
+import { Estudiante } from '../../utils/interface/Estudiante'
+
+const route = '/estudiantes'
+
+export class EstudianteService {
+  EstudianteService(){
+
+  }
+
+  async getAll(): Promise<IReturn<Estudiante[]>> {
+    try {
+      const { data } = await esfeapi.get<Estudiante[]>(route)
+      console.log(data)
+      return returnProvider(data, 'Estudiantes fetched', true)
+    } catch (error) {
+      return returnProvider([], String(error), false)
+    }
+  }
+
+  async getOne(id: number): Promise<IReturn<Estudiante | null>>{
+    try {
+      const { data } = await esfeapi.get<Estudiante[]>(route)
+      if(!data) return returnProvider(null, 'error al obtener Estudiante', false)
+  
+      const Estudiante = data.filter( Estudiante => Estudiante.id === id)[0]
+  
+      if(!Estudiante) return returnProvider(null, 'no existen un Estudiante', false)
+      return returnProvider(Estudiante, 'Estudiante obtenido', true)
+      
+    } catch (error) {
+      return returnProvider(null, String(error), false)
+    }
+  }
+}
---
+++ src/utils/interface/Estudiante.ts
@@ -0,0 +1,7 @@
+export interface Estudiante {
+  id: number,
+  nombre: string,
+  correo: string,
+  telefono: string,
+  grupoId: number
+}
