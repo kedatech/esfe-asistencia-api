cb4a43c5fb9a19f444d9271815332a2f13780ed1
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-08-22 03:26:33 -0600

    chore: :sparkles: crud rfid services

--- src/services/rfid.services.ts
+++ src/services/rfid.services.ts
@@ -1,6 +1,7 @@
 import { CreateInput, UpdateInput} from '../schemas/rfid.schemas'
 import { prisma } from '../lib/prisma'
 import { uidRFIDValidator } from '../utils/validators/isRFID'
+import { returnProvider } from '../utils/providers/ReturnProvider'
 import { EstudianteService } from './external/estudiante.services'
 
 const estudianteService = new EstudianteService()
@@ -16,21 +17,33 @@ export class CicloClase {
   }
 
   async create(data: CreateInput){
-    const estudiantes = await estudianteService.getAll();
+    try {
+      if(!await this.validData(data)) returnProvider(null, 'Datos incorrectos', false)// validar uuid valido y que exista estudiante
 
-    const validUid = uidRFIDValidator.safeParse(data.uid).success
+      const result = await prisma.rfid.create({
+        data: data
+      })
+      return returnProvider(result, 'RFID uid guardado', true)
 
-    const queryEstudiante = estudiantes.data.filter( el => el.id == data.estudianteId)
-    return prisma.rfid.create({
-      data: data
-    })
+    } catch (error) {
+      return returnProvider([], String(error), false)
+    }
+    
   }
 
-  update(id: number, data: UpdateInput){
-    return prisma.rfid.update({
-      where: { id: id},
-      data
-    })
+  async update(id: number, data: UpdateInput){
+    try {
+      if(!uidRFIDValidator.safeParse(data.uid).success) return false
+
+      const result = prisma.rfid.update({
+        where: { id: id},
+        data
+      })
+      
+      return returnProvider(result, 'UUID actualizado', true)
+    } catch (error) {
+      return returnProvider([], String(error), false)
+    }
   }
 
   delete(id: number){
@@ -38,4 +51,13 @@ export class CicloClase {
       where: { id: id }
     })
   }
+
+  async validData(data:CreateInput){
+    const estudiantes = await estudianteService.getAll();
+    const validUid = uidRFIDValidator.safeParse(data.uid).success
+    const queryEstudiante = estudiantes.data.filter( el => el.id == data.estudianteId)
+    if(!validUid && !queryEstudiante) return false
+    return true
+  }
 }
+
