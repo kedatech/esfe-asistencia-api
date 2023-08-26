2770c134ad52680b72d6f698d8ecb245030d0031
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-08-26 14:18:12 -0600

    feat: :sparkles: create route, schema and services estudiante

---
+++ src/routes/estudiante.routes.ts
@@ -0,0 +1,44 @@
+import { Router, Request, Response } from 'express'
+import { getSchema } from '../schemas/estudiante.schemas'
+import { validate } from '../../middlewares/validate'
+import { EstudianteService } from '../services/external/estudiante.services' 
+
+const service = new EstudianteService()
+
+const router = Router()
+
+router.get('/', async (req: Request, res: Response) => {
+  try{
+    const data = await service.getAll()
+    res.json(data)
+  }catch (error){
+    res.json(error)
+  }
+})
+
+router.get('/:id',
+validate(getSchema),
+async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id)
+    const del = await service.getOne(id)
+    res.json(del)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+router.get('/docente/:id',
+validate(getSchema),
+async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id)
+    console.log('docente id',id)
+    const del = await service.getByDocente(id)
+    res.json(del)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
+export { router }
